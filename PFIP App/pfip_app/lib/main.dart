799060ee9ae85633f5523f82b8048336cde6ca9b
Dave Benemerito <iamdavebenemerito@gmail.com>
2023-11-13 15:58:19 +0800

    Added Graph and kWH Pricing

--- PFIP App/pfip_app/lib/main.dart
+++ PFIP App/pfip_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:pfip_app/colors.dart';
 import 'package:pfip_app/firebase_options.dart';
@@ -43,7 +44,12 @@ class _MyHomePageState extends State<MyHomePage> {
   double energyReading = 0;
   double currentReading = 0;
   double powerReading = 0;
+  double energyBill = 0;
+  double kwhPrice = 0;
   int tempReading = 0;
+  List<int> voltageDataPoints = []; //LUX
+  List<double> powerDataPoints = [];
+  final int maxDataPoints = 20;
   FirebaseDatabase database = FirebaseDatabase.instance;
 
   bool buttonToggle = false;
@@ -52,34 +58,89 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     DatabaseReference liveData = database.ref('/device-live');
+    DatabaseReference paramsData = database.ref('/device-params');
+    DatabaseReference recordsData = database.ref('/device-records');
+
+    recordsData.onChildAdded.listen((DatabaseEvent event) {
+      final vol =
+          int.parse(event.snapshot.child('vol-reading').value.toString());
+      final curr =
+          double.parse(event.snapshot.child('pow-reading').value.toString());
+
+      setState(() {
+        voltageDataPoints.add(vol);
+        powerDataPoints.add(curr);
+        if (voltageDataPoints.length > maxDataPoints) {
+          voltageDataPoints.removeAt(0); // Remove the oldest data point.
+          powerDataPoints.removeAt(0);
+        }
+      });
+    });
 
     liveData.onValue.listen((DatabaseEvent event) {
-      final data = event.snapshot.value;
+      final temp =
+          int.parse(event.snapshot.child('temp-reading').value.toString());
+      final volt =
+          int.parse(event.snapshot.child('vol-reading').value.toString());
+      final power =
+          double.parse(event.snapshot.child('pow-reading').value.toString());
+      final current =
+          double.parse(event.snapshot.child('curr-reading').value.toString());
+      final ener =
+          double.parse(event.snapshot.child('ener-reading').value.toString());
+      // final price =
+      //     typedData['device-params/kwh-price'] is double ? typedData['device-params/kwh-price'] as double : 0.0;
 
-      if (data != null && data is Map<Object?, Object?>) {
-        final Map<String, dynamic> typedData = data.cast<String, dynamic>();
-        final temp =
-            data['temp-reading'] is int ? data['temp-reading'] as int : 0;
-        final volt =
-            data['vol-reading'] is int ? data['vol-reading'] as int : 0;
-        final power =
-            data['pow-reading'] is double ? data['pow-reading'] as double : 0.0;
-        final current = data['curr-reading'] is double
-            ? data['curr-reading'] as double
-            : 0.0;
-        final ener = data['ener-reading'] is double
-            ? data['ener-reading'] as double
-            : 0.0;
-
-        setState(() {
-          tempReading = temp;
-          voltReading = volt;
-          powerReading = power;
-          currentReading = current;
-          energyReading = ener;
-        });
-      }
+      setState(() {
+        tempReading = temp;
+        voltReading = volt;
+        powerReading = power;
+        currentReading = current;
+        energyReading = ener;
+        energyBill = kwhPrice * energyReading;
+      });
     });
+
+    paramsData.onValue.listen((DatabaseEvent event) {
+      final price =
+          double.parse(event.snapshot.child('set-price').value.toString());
+
+      setState(() {
+        kwhPrice = price;
+        energyBill = kwhPrice * energyReading;
+      });
+    });
+  }
+
+  void _showEditDialog(BuildContext context) {
+    TextEditingController priceController = TextEditingController();
+    priceController.text = kwhPrice.toString(); // Set initial value
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Edit Price'),
+          content: TextField(
+            controller: priceController,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(labelText: 'Enter new price'),
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                
+                database
+                    .ref('device-params/')
+                    .update({"set-price": double.parse(priceController.text)});
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   readingsWidget() {
@@ -261,8 +322,148 @@ class _MyHomePageState extends State<MyHomePage> {
 
   graphPage() {
     return Center(
-        child:
-            Column(mainAxisAlignment: MainAxisAlignment.center, children: []));
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Row(
+          children: [
+            SizedBox(width: 30),
+            SizedBox(
+              height: 10,
+              width: 10,
+              child: DecoratedBox(
+                  decoration: BoxDecoration(color: Colors.yellow[600])),
+            ),
+            Text(
+              " Voltage   ",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 10),
+            ),
+            SizedBox(
+              height: 10,
+              width: 10,
+              child:
+                  DecoratedBox(decoration: BoxDecoration(color: Colors.blue)),
+            ),
+            Text(
+              " Power  ",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 10),
+            ),
+          ],
+        ),
+        Container(
+            height: 200,
+            padding: EdgeInsets.all(15),
+            child: LineChart(
+              LineChartData(
+                  maxY: 300,
+                  minY: 0,
+                  gridData: FlGridData(show: false),
+                  borderData: FlBorderData(show: true),
+                  lineBarsData: [
+                    LineChartBarData(
+                      preventCurveOverShooting: true,
+                      spots: voltageDataPoints.asMap().entries.map((entry) {
+                        return FlSpot(
+                            entry.key.toDouble(), entry.value.toDouble());
+                      }).toList(),
+                      isCurved: true,
+                      barWidth: 2,
+                      color: Colors.yellow[600],
+                      dotData: FlDotData(show: false),
+                      belowBarData: BarAreaData(show: false),
+                    ),
+                    LineChartBarData(
+                      spots: powerDataPoints.asMap().entries.map((entry) {
+                        return FlSpot(
+                            entry.key.toDouble(), entry.value.toDouble());
+                      }).toList(),
+                      isCurved: true,
+                      barWidth: 2,
+                      color: Colors.blue,
+                      dotData: FlDotData(show: false),
+                      belowBarData: BarAreaData(show: false),
+                    )
+                    // LineChartBarData(
+                    //   spots: [
+                    //     FlSpot(dataPoints.elementAt(9).time.toDouble(),
+                    //         saveLux.toDouble())
+                    //   ],
+                    //   isCurved: false,
+                    //   barWidth: 2,
+                    //   color: Colors.red,
+                    //   dotData: FlDotData(show: true),
+                    //   belowBarData: BarAreaData(show: false),
+                    // ),
+                  ],
+                  titlesData: FlTitlesData(
+                      topTitles:
+                          AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                      rightTitles:
+                          AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                      leftTitles: AxisTitles(
+                          // axisNameWidget: Text(
+                          //   "Voltage",
+                          //   style: TextStyle(
+                          //       fontWeight: FontWeight.bold, fontSize: 10),
+                          // ),
+                          sideTitles: SideTitles(
+                              getTitlesWidget: (value, meta) => Text(
+                                  value.toInt().toString(),
+                                  style: TextStyle(fontSize: 9)),
+                              interval: 50,
+                              showTitles: true)),
+                      bottomTitles: AxisTitles(
+                          sideTitles: SideTitles(
+                        // getTitlesWidget: (value, meta) {
+                        //   DateTime date =
+                        //       DateTime.fromMillisecondsSinceEpoch(
+                        //           value.toInt());
+                        //   String formattedTime =
+                        //       DateFormat("h:mm").format(date);
+                        //   return Text(
+                        //     formattedTime,
+                        //     style: TextStyle(fontSize: 9),
+                        //   );
+                        // },
+                        showTitles: false,
+                      )))),
+            )),
+        SizedBox(
+          height: 15,
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: 300,
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey, width: 1),
+              borderRadius: BorderRadius.circular(20)),
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Text('Est. Monthly Bill',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    )),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    // Open dialog for editing the price
+                    _showEditDialog(context);
+                  },
+                  child: Text('Edit'),
+                )
+              ]),
+              Text('₱${energyBill.toStringAsFixed(2)}',
+                  style: const TextStyle(
+                      color: primary,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 40))
+            ],
+          ),
+        ),
+      ]),
+    );
   }
 
   setPage() {
@@ -296,7 +497,9 @@ class _MyHomePageState extends State<MyHomePage> {
                 setState(() {
                   buttonToggle = !buttonToggle;
                 });
-                database.ref('device-params/').update({'set-switch' : buttonToggle});
+                database
+                    .ref('device-params/')
+                    .update({'set-switch': buttonToggle});
               },
             )),
       ]),
